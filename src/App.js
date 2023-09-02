841b07bf90377dede1c6aa917b927c091bd706de
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-09-02 14:56:38 +0430

    adding assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    </>
+  )
 }
 
 export default App;
+
+
+// 93a984c2321bd24125311f8482e7f687
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={93a984c2321bd24125311f8482e7f687}

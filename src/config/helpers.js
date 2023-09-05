67f70bd527a4df7b9278dc84368aba2bc0e38367
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-09-05 16:38:37 +0430

    adding index.js file

--- src/config/helpers.js
+++ src/config/helpers.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
 
-
+// Get an array and return a random element
 const randSelect = (ImageArray) => {
     const selector = Math.floor(Math.random * ImageArray.length);
     return ImageArray[selector];
 }
 
 
-axios.get('https://api.openweathermap.org/data/2.5/weather?q=herat&appid=93a984c2321bd24125311f8482e7f687').then(function getWeather (res){
-    console.log("");
+
+const getWeather = (city)=>{
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93a984c2321bd24125311f8482e7f687`).
+    then(function getWeather (res){
+    console.log(res);
 })
+}
+
